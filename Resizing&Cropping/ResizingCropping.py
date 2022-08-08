ebf1feccdc1d519acb85f2c1a5b050ba2453ba6e
turkanbasut <89381428+turkanbasut@users.noreply.github.com>
2022-08-08 17:59:33 +0300

    added Resizing and Cropping image with openCV

---
+++ Resizing&Cropping/ResizingCropping.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("Resources/alice.png")
+print(img.shape)
+
+imgResize = cv2.resize(img, (300, 200))
+print(imgResize.shape)
+
+imgCropped = img[0:200, 200:500]
+
+
+cv2.imshow("Image", img)
+cv2.imshow("Image Resize", imgResize)
+cv2.imshow("Image Cropped", imgCropped)
+cv2.waitKey(0)
